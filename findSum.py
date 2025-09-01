ddfbb4acf1910166c76390956704889b35abd969
Deeksha Phaneendra Reddy Ummadi (dummadi) <dummadi@cisco.com>
2025-09-01 10:57:25 +0530

    ADD sample python code

---
+++ findSum.py
@@ -0,0 +1,12 @@
+#find sum
+'''
+    findSum 
+    args : firstNumber , secondNumber
+''' 
+
+def findSum(firstNumber, secondNumber):
+    sum = firstNumber + secondNumber
+    return sum
+
+print(findSum(10, 20))
+print(findSum(1, 2))
